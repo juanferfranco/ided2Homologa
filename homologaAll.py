6d31d92499b81df1faba62da826699512a8946ce
Juan Franco <juanferfranco@gmail.com>
2021-06-04 16:21:43 -0500

    Homologa Robot

---
+++ homologaAll.py
@@ -0,0 +1,42 @@
+from seleniumTest import SeleniumRobot
+import homologaFunc
+import pandas as pd
+import os
+
+########################################
+# Reads students list
+########################################
+data = pd.read_excel ("./listado.xlsx")
+listado = data.values
+
+
+########################################
+# Loads teacher's sigaa credentials
+########################################
+
+sigaa = pd.read_json("./sigaa.json",typ = "series")
+sigaaId = sigaa.values[0]
+sigaaPassword = sigaa.values[1]
+
+########################################
+# Logins in the sigaa system
+########################################
+robot = SeleniumRobot()
+robot.setup_method()
+robot.initCapp(sigaaId, sigaaPassword)
+
+########################################
+# Reads the capp for each student and 
+# draws his curriculum
+########################################
+
+for item in listado:
+    name = item[0]
+    id = str(item[1]).zfill(9)
+    line = item[4]
+    robot.getCapp(id)
+    homologaFunc.homologa(name,id,line)
+    if os.path.exists("./capp.html"): 
+        os.remove("./capp.html")
+    else:
+        print("{} capp.html does not exist".format(id))
